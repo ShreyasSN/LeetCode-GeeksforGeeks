7ab9c9ffc6386fd8fa556cf714ab896e227b17d3
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2023-06-09 22:17:59 +0530

    Time: 25 ms (7.18%), Space: 15.9 MB (72.43%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -5,8 +5,8 @@ public:
         while(left <= right)
         {
             mid = left + (right - left ) /2;
-            if(letters[mid] <= target) left = mid+1;
-            else right = mid- 1;
+            if(letters[mid] <= target) left = mid + 1;
+            else right = mid - 1;
         }
         return left == letters.size() ? letters[0] : letters[left];
     }
