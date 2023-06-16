709f5c59dca3caee9007429177efbc9f6e3cf682
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2023-06-16 16:12:50 +0530

    Time: 20 ms (25.04%), Space: 15.8 MB (70.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,11 +4,9 @@ public:
         int count = (nums.size())/3;
         unordered_map<int, int> mp;
         set<int>res;
-        for(auto x : nums){
-            mp[x]++;
-        }
         for(int i=0; i<nums.size(); i++)
         {
+            mp[nums[i]]++;
             if(mp[nums[i]] > count) res.insert(nums[i]);
         }
         vector<int>ans(res.begin(), res.end());
