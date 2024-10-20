66d2693cdbe99b9ae18a3501f3b8f12eb6d8ffaf
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2024-10-20 10:05:15 +0530

    Time: 19 ms (100%), Space: 141.8 MB (39.13%) - LeetHub

---
+++ 3326-minimum-division-operations-to-make-array-non-decreasing/3326-minimum-division-operations-to-make-array-non-decreasing.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0;
+        for (int i = nums.size() - 1; i > 0; --i) {
+            if (nums[i] < nums[i - 1]) {
+                nums[i - 1] = findNum(nums[i], nums[i - 1]);
+                if (nums[i - 1] == -1) return -1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+    
+    int findNum(int n1, int n2) {
+        for (int i = n1; i > 1; --i) {
+            if (n2 % i == 0) return i;
+        }
+        return -1;
+    }
+};
