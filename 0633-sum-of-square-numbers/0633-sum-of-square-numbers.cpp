dde23ce05d47b14ac4399906d65633234576252f
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2025-02-06 20:14:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        for(long long i=0, j=sqrt(c); i<=j;){
+        for(long long i=0, j=c; i<=j;){
             if((i*i + j*j) == c) return true;
             ((i*i + j*j) < c) ? i++ : j--;
         }
