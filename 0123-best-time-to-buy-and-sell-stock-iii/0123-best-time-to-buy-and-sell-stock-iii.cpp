ff96ade491d3c4e3355671d930a6964743c97fe4
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2025-03-17 15:00:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,14 +1,26 @@
 class Solution {
+    static const int N = 1e5+1;
+    int dp[N][2][2];
+    int maxdepth = 0;
+    int f(vector<int>& p, int i, int buy, int k, int deep = 0){
+        if(i == p.size() || k == 0) return 0;
+        maxdepth = max(maxdepth, deep);
+        int &res = dp[i][buy][k];
+        if(res != -1) return res;
+        int sell = INT_MIN, purchase = INT_MIN, exc = 0;
+        if(buy) {
+            purchase = f(p, i+1, 0, k, deep+1) - p[i];
+        } else {
+            sell = f(p, i+1, 1, k-1, deep+1) + p[i];
+        }
+        exc = f(p, i+1, buy, k, deep+1);
+        return res = max({sell, purchase, exc});
+    }
 public:
     int maxProfit(vector<int>& prices) {
-        int hold1 = INT_MIN, hold2 = INT_MIN;
-        int free1 = 0, free2 = 0;
-        for(int x: prices){
-            free2 = max(free2, hold2+x);
-            hold2 = max(hold2, free1-x);
-            free1 = max(free1, hold1+x);
-            hold1 = max(hold1, -x);
-        }
-        return free2;
+        memset(dp, -1, sizeof(dp));
+        int res = f(prices, 0, 1, 2);
+        cout << maxdepth << '\n';
+        return res;
     }
 };
