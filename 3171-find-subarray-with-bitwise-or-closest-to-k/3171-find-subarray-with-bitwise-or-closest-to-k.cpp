8468c47bc1c4816b9cb1d694cb0ae9c283074f42
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2025-05-12 17:12:10 +0530

    Time: 1451 ms (20%), Space: 107.8 MB (58.79%) - LeetHub

--- 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.cpp
+++ 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.cpp
@@ -1,38 +1,55 @@
-class Solution {
-    static const int N = 1e5;  
-    int n; 
-    int t[2 * N];
+const int N = 3e5 + 9;
 
-    void build() {  
-        for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
-    }
+int a[N];
+struct ST {
+    int t[2 * N]; 
+    ST() { memset(t, 0, sizeof t); }
 
-    int query(int l, int r) {
-        int res = 0;
-        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if (l&1) res += t[l++];
-            if (r&1) res += t[--r];
+    int merge(int l, int r) { return l | r; }
+
+    void build(int n, int b, int e) {
+        if (b > e) return;
+        if (b == e) {
+            t[n] = a[b];
+            return;
         }
-        return res;
+        int mid = (b + e) >> 1, lft = n << 1, rgt = lft | 1;
+        build(lft, b, mid);
+        build(rgt, mid + 1, e);
+        t[n] = merge(t[lft], t[rgt]);
     }
+    
+    int query(int n, int b, int e, int i, int j) {
+        if (b > j || e < i) return 0;
+        if (b >= i && e <= j) return t[n];
+        int mid = (b + e) >> 1, lft = n << 1, rgt = lft | 1;
+        int L = query(lft, b, mid, i, j);
+        int R = query(rgt, mid + 1, e, i, j);
+        return merge(L, R);
+    }
+} T;
+
+class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
-        n = nums.size();
-        for (int i = 0; i < n; i++) t[i + n] = nums[i];
-        build();
+        int n = nums.size();
+        for (int i = 0; i < n; i++) a[i] = nums[i];
+
+        T.build(1, 0, n - 1);
 
         int ans = INT_MAX;
         for (int l = 0; l < n; ++l) {
             int lo = l, hi = n - 1, best = INT_MAX;
             while (lo <= hi) {
                 int mid = (lo + hi) >> 1;
-                int cur = query(l, mid+1);
+                int cur = T.query(1, 0, n - 1, l, mid);
                 ans = min(ans, abs(k - cur));
                 if (cur == k) return 0;
                 if (cur > k) hi = mid - 1;
                 else lo = mid + 1;
             }
         }
+
         return ans;
     }
 };
