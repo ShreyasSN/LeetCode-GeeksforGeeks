ba34626f4009ad7979d1fbe878a37114b38752d4
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2025-05-31 17:15:54 +0530

    Time: 907 ms (5%), Space: 263.8 MB (26.58%) - LeetHub

--- 3528-unit-conversion-i/3528-unit-conversion-i.cpp
+++ 3528-unit-conversion-i/3528-unit-conversion-i.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     vector<int> baseUnitConversions(vector<vector<int>>& con) {
         int n = con.size();
-        int mod = (int)1e9+7;
-        vector<vector<pair<int,int>>> g(n+1);
-        for(auto e:con) {
+        int mod = (int)1e9 + 7;
+        vector<vector<pair<int, int>>> g(n + 1);
+
+        for (auto& e : con) {
             g[e[0]].push_back({e[1], e[2]});
         }
-        queue<int> q;
-        q.push(0);
-        vector<int> res(n+1, -1);
+
+        vector<int> res(n + 1, -1);
+        vector<bool> vis(n + 1, false);
         res[0] = 1;
-        while(!q.empty()) {
-            int u = q.front();
-            q.pop();
-            for(auto [v, f]:g[u]) {
-                if(res[v] == -1) {
-                    res[v] = (1ll * res[u] * f) % mod;
-                    q.push(v);  
+
+        function<void(int)> dfs = [&](int u) {
+            vis[u] = true;
+            for (auto& [v, c] : g[u]) {
+                if (!vis[v]) {
+                    res[v] = (1LL * res[u] * c) % mod;
+                    dfs(v);
                 }
             }
-        }
+        };
+
+        dfs(0);
         return res;
     }
 };
