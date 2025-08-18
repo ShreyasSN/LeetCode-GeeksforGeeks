846ab58824d9be5c863d203788b31ed9addab9c7
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2025-08-18 13:55:55 +0530

    Time: 483 ms (55.12%), Space: 172.8 MB (50.2%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,46 +1,22 @@
-int n;
-struct node {
-    int val;
-    node() : val(0) {}
-    node(int v) : val(v) {}
-};
-node t[2 * (1<<17)]{}; 
-node merge(node l, node r) {
-    return node(l.val + r.val);
-}
-void build() {
-    for (int i = n - 1; i > 0; --i)
-        t[i] = merge(t[i<<1], t[i<<1|1]);
-}
-void update(int p, int value) {
-    for (t[p += n] = node(value); p > 1; p >>= 1)
-        t[p>>1] = merge(t[p], t[p^1]);
-}
-node query(int l, int r) {
-    node resl(0), resr(0);
-    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-        if (l&1) resl = merge(resl, t[l++]);
-        if (r&1) resr = merge(t[--r], resr);
-    }
-    return merge(resl, resr);
-}
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
+                         tree_order_statistics_node_update>;
 
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        n = nums.size();
+        int n = nums.size();
+        vector<int> ans(n);
+        ordered_set<pair<int,int>> os; 
 
-        map<int, int> mp;
-        int idx = 0;
-        for (int x : nums) mp[x];
-        for (auto &kv : mp) kv.second = idx++; 
-
-        vector<int> res(n);
         for (int i = n - 1; i >= 0; --i) {
-            int pos = mp[nums[i]];
-            res[i] = query(0, pos).val;
-            update(pos, 1);
+            ans[i] = os.order_of_key({nums[i], i});
+            os.insert({nums[i], i});
         }
-        return res;
+        return ans;
     }
 };
