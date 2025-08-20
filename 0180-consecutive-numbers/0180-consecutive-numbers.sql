c30b9012610d294806bc09375b3c550c5b5ddbcf
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2025-08-20 16:53:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,6 @@
+select num as ConsecutiveNums from (
+    select num, lag(num, 1) over (order by id asc) as lg, 
+                lead(num, 1) over (order by id asc) as ld
+                from logs
+) as sub
+where num = lg and num = ld
