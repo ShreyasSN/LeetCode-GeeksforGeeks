86f92e3de2749469f16a9fb8b0ce8e1007f39d24
Shreyas S N <91681619+ShreyasSN@users.noreply.github.com>
2025-08-25 16:46:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,48 +1,12 @@
-#define inf 0x3f3f3f3f
 class Solution {
-    static const int N = 1 << 17;
-    int n;
-
-    struct vertice {
-        int64_t pref, suf, sum, maxSeg;
-
-        vertice() : pref(-inf), suf(-inf), sum(-inf), maxSeg(-inf) {}
-        vertice(int64_t v) : pref(v), suf(v), sum(v), maxSeg(v) {}
-
-        static vertice combine(const vertice &l, const vertice &r) {
-            vertice res;
-            res.pref = max(l.pref, l.sum + r.pref);
-            res.suf = max(r.suf, r.sum + l.suf);
-            res.sum = l.sum + r.sum;
-            res.maxSeg = max({l.maxSeg, r.maxSeg, l.suf + r.pref});
-            return res;
-        }
-    };
-
-    vertice t[2 * N];
-
-    void build() {
-        for (int i = n - 1; i > 0; --i) {
-            t[i] = vertice::combine(t[i * 2], t[i * 2 + 1]);
-        }
-    }
-
-    vertice query(int l, int r) {
-        vertice resl(-inf), resr(-inf);
-        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if (l & 1) resl = vertice::combine(resl, t[l++]);
-            if (r & 1) resr = vertice::combine(t[--r], resr);
-        }
-        return vertice::combine(resl, resr);
-    }
-
 public:
     int maxSubArray(vector<int>& nums) {
-        n = (int)nums.size();
-        for (int i = 0; i < n; ++i) {
-            t[n + i] = vertice(nums[i]);
+        int n=nums.size();
+        int cur=INT_MIN,mx=INT_MIN;
+        for(int& i:nums){
+            cur=max(cur+i,i);
+            mx=max(cur,mx);
         }
-        build();  
-        return query(0, n).maxSeg; 
+        return mx;
     }
 };
